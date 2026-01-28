035d011bb0086a3475675378b9825aacd362e69e
danielfrey63 <daniel.frey@sbb.ch>
2026-01-28 16:59:01 +0100

    fix(secrets): support utf-8 and utf-16 passwords

--- secrets/decrypt.sh
+++ secrets/decrypt.sh
@@ -16,12 +16,14 @@ tmpzip="$(mktemp -t secrets.XXXXXX).zip"
 read -s -p "Passwort: " pw
 echo
 
-# Key/IV ableiten (PBKDF2-HMAC-SHA256, 200000 Iterationen, Salt=erste 16 Bytes, dkLen=48 => 32 Key + 16 IV)
-read -r key_hex iv_hex < <(
-python3 - <<'PY' "$in" "$pw"
+derive_key_iv() {
+  local enc="$1"
+  python3 -W "ignore::DeprecationWarning" - <<'PY' "$in" "$pw" "$enc"
 import sys, binascii, hashlib
 path = sys.argv[1]
-pw = sys.argv[2].encode("utf-16-le")
+pw_s = sys.argv[2]
+enc = sys.argv[3]
+pw = pw_s.encode(enc)
 with open(path, "rb") as f:
     salt = f.read(16)
 dk = hashlib.pbkdf2_hmac("sha256", pw, salt, 200000, dklen=48)
@@ -29,10 +31,28 @@ key = dk[:32]
 iv  = dk[32:]
 print(binascii.hexlify(key).decode(), binascii.hexlify(iv).decode())
 PY
-)
+}
+
+try_decrypt() {
+  local enc="$1"
+  read -r key_hex iv_hex < <(derive_key_iv "$enc")
+  tail -c +17 "$in" | openssl enc -aes-256-cbc -d -K "$key_hex" -iv "$iv_hex" -out "$tmpzip"
+}
 
 # Ciphertext (ab Byte 17) mit OpenSSL entschluesseln -> ZIP
-tail -c +17 "$in" | openssl enc -aes-256-cbc -d -K "$key_hex" -iv "$iv_hex" -out "$tmpzip"
+set +e
+try_decrypt "utf-8"
+rc=$?
+if [[ $rc -ne 0 ]]; then
+  try_decrypt "utf-16-le"
+  rc=$?
+fi
+set -e
+
+if [[ $rc -ne 0 ]]; then
+  echo "Fehler: Entschluesselung fehlgeschlagen (UTF-8 und UTF-16LE probiert)." >&2
+  exit $rc
+fi
 
 # ZIP entpacken
 unzip -oq "$tmpzip" -d "$outdir"
